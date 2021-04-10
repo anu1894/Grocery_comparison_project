54460336693f4b4f1b8d64b21c2fc4435e852670
Anushka <x19238592@student.ncirl.ie>
2021-04-10 11:56:24 +0000

    grocery_comparison_project

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ec2-user/environment/grocery_comparison_project/PicknEat/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
